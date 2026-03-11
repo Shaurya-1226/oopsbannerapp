1845258a885a59d94914bd35e04835399c296bdc
Shaurya <shaurya.singla2006@gmail.com>
2026-03-11 14:08:00 +0530

    UC2

---
+++ UC2.java
@@ -0,0 +1,11 @@
+public class UC2 {
+	public static void main (String arg[]){
+		System.out.println("   ***   " + " " + "   ***   " + " " + "****** " + " " +  "   *****");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "*    **" + " " +  " **     ");
+		System.out.println("**     **" + " " + "**     **" + " " + "*     *" + " " +  "**      ");
+		System.out.println("**     **" + " " + "**     **" + " " + "****** " + " " +  "  ***** ");
+		System.out.println("**     **" + " " + "**     **" + " " + "*      " + " " +  "      **");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "*      " + " " +  "**    **");
+		System.out.println("   ***   " + " " + "   ***   " + " " + "*      " + " " +  " ****** ");
+	}
+}
